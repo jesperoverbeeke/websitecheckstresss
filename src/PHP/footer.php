9dcab33c6a6f0e7c58afcedd7186dc27bc748e05
jesperoverbeeke <jespo9@hotmail.nl>
2016-03-04 15:55:37 +0100

    Fix stuff

---
+++ src/PHP/footer.php
@@ -0,0 +1,19 @@
+<style>
+#footer {
+    background-color:black;
+    color:white;
+    clear:both;
+    text-align:center;
+    padding:5px; 
+	position:absolute;
+	bottom:0;
+	height:50px;
+	margin:0;
+	right:0;
+	left:0;
+	
+}
+</style>
+<div id="footer">
+<p>Copyright wij</p>
+</div>
---
+++ src/pages/Ambitie/Index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Ambitie</title>
+        <link rel="stylesheet" type="text/css" href="/Test_pagina/css/main.css"/>
+        
+    </head>
+    <body>
+    <?php include($_SERVER["DOCUMENT_ROOT"]."/Test_pagina/PHP/header.php"); ?>
+    <div id="nav">
+	<p>submenu<br>hallo<br>
+	</div>
+    <div id="body">
+     
+       <p> En hier dan de pagin
+	   -inhouEn hier dan de pagina-inhoud
+	   En hier dan de pagina-inhoudEn hier dan 
+	   de pagina-inhoudEn hier dan de pagina-inhoud
+	   En hier dan de pagina-inhoudEn hier dan de 
+	   pagina-inhoud
+	   d.
+	   </p>
+    </div>
+	
+    <?php include($_SERVER["DOCUMENT_ROOT"]."/Test_pagina/PHP/footer.php"); ?>
+    </body>
+</html>
